898d991a2e99ecd925cf98dd1f8eb6ca3f30968f
Stefan <stefan5andonov@yahoo.com>
2017-03-21 19:27:04 +0100

    First commit

---
+++ src/k1g1.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.List;
+import java.util.Scanner;
+
+public class k1g1 {
+	
+	/* 
+	 * Site tekstualni datoteki od imenikot from gi premestuva vo imenikot to
+	 * Ako imenikot from ne postoi pecati "Ne postoi"
+	 * Ako imenikot to ne postoi, go kreira.	 *   
+	 */	
+	
+	public static void moveWritableTxtFiles(String from, String to) throws IOException {
+		File source = new File(from);
+		
+		if (!source.exists()){
+			System.out.println("Ne postoi");
+			return;
+		}
+		
+		File destination = new File(to);
+		
+		if (!destination.exists()){
+			destination.mkdirs();
+		}
+		
+		File[] files = source.listFiles(new FilenameFilter(){
+			@Override
+			public boolean accept(File dir, String name) {
+				File f = new File(name);
+				String s = f.getName();
+				return dir.isDirectory() || ( f.canWrite() && s.endsWith(".txt"));						
+			}
+		});
+		
+		for (File f : files){
+			if (!f.isDirectory())
+				f.renameTo(new File(destination.getAbsolutePath()+"\\"+f.getName()));
+			else
+				moveWritableTxtFiles(f.getAbsolutePath(),destination.getAbsolutePath());				
+		}
+			
+		
+	}
+	
+	/*
+	 * Sodrzinata na datotekata source (koja ima ogromen broj na podatoci), sekoj od niv so ista dolzina,
+	 * ja vcituva i ja vnesuva vo lista od niza od bajti.
+	 */
+	
+	static void deserializeData(String source, List<byte[]> data, long elementLength){
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String from = sc.nextLine();
+		String to = sc.nextLine();
+		
+		moveWritableTxtFiles(from,to);
+		
+		
+		
+
+	}
+
+}
